b3bab51ba1d90a27c5e6a88771d044425518e9e8
Daan de Vries <daan.de.vries@icloud.com>
2020-08-05 13:59:00 +0200

    add preview server

---
+++ preview/startup.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const apiMocker = require('connect-api-mocker');
+const path = require('path');
+
+const app = express();
+
+app.use(apiMocker('/api', path.resolve(__dirname, '/mocks/api')));
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+app.listen(8080);
