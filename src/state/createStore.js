e1473769c4c4dfb34a03e49b1450e7084481920f
Daan de Vries <daan.de.vries@icloud.com>
2020-08-05 11:01:04 +0200

    rename modules.js to createRootReducer.js

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { routerMiddleware } from 'connected-react-router'
 
-import createRootReducer from './modules';
+import createRootReducer from './createRootReducer';
 
 export default history => {
   const middleware = [
