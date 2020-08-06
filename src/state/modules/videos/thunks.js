c61e476288697d9788f59e80541cec9cdf598528
Daan de Vries <daan.de.vries@icloud.com>
2020-08-06 18:16:21 +0200

    add thest for videos thunk

--- src/state/modules/videos/thunks.js
+++ src/state/modules/videos/thunks.js
@@ -1,14 +1,12 @@
-// Thunks
 import {apiGet} from "./apiRequest";
 import {NAMESPACE, VIDEOS_ENDPOINT} from "./const";
 import {hydrateVideoData} from "./actions";
 
 const FETCH_VIDEOS = `${NAMESPACE}/FETCH_VIDEOS`;
 
-export const fetchVideos = () => dispatch => {
-    return dispatch(apiGet(FETCH_VIDEOS, VIDEOS_ENDPOINT)).payload.then(
-        response => {
-            dispatch(hydrateVideoData(response.data));
-        },
-    );
-};
+export const fetchVideos = () => dispatch => dispatch(apiGet(FETCH_VIDEOS, VIDEOS_ENDPOINT)).payload.then(
+    response => {
+        dispatch(hydrateVideoData(response.data));
+    },
+);
+
