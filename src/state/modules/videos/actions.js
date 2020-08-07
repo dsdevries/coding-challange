c6e289b33825829f4193a829b5671a50f8b5d081
Daan de Vries <daan.de.vries@icloud.com>
2020-08-07 15:32:03 +0200

    fix bad commit

--- src/state/modules/videos/actions.js
+++
@@ -1,4 +0,0 @@
-import {HYDRATE_VIDEO_DATA} from "./const";
-import {createAction} from "redux-actions";
-
-export const hydrateVideoData = createAction(HYDRATE_VIDEO_DATA);
--- src/state/modules/videos/const.js
+++
@@ -1,4 +0,0 @@
-export const NAMESPACE = 'videos';
-export const VIDEOS_ENDPOINT = '/videos';
-export const PAGE_SIZE = 5;
-export const HYDRATE_VIDEO_DATA = `${NAMESPACE}/HYDRATE_VIDEO_DATA`;
